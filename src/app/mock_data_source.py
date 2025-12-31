1a4a5e2e685324cd1168b2007313d64197ecd87c
Joe Chan <firstianus@gmail.com>
2025-12-31 10:55:54 -0500

    Cosmetic tweaks.

--- src/app/mock_data_source.py
+++ src/app/mock_data_source.py
@@ -12,10 +12,10 @@ from aws_cost_tool.cost_explorer import DateRange
 class Services(StrEnum):
     _weight: int
 
-    EC2 = ("EC2", 100)
+    EC2 = ("Amazon Elastic Compute Cloud", 100)
     EC2_OTHER = ("EC2 - Other", 30)
-    S3 = ("S3", 20)
-    RDS = ("RDS", 80)
+    S3 = ("Amazon Simple Storage Service", 20)
+    RDS = ("Amazon Relational Database Service", 80)
     LAMBDA = ("Lambda", 5)
     CLOUDWATCH = ("CloudWatch", 10)
     DYNAMODB = ("DynamoDB", 15)
@@ -160,7 +160,7 @@ def _generate_mock_ec2_other_usage(
     )
     data.append(vpc_usage)
 
-    # Add some samll fake misc items, outside of the well-known categories.
+    # Add some small fake misc items, outside of the well-known categories.
     other_usage = _generate_mock_usage_data(
         ranges,
         "EC2 - Other",
